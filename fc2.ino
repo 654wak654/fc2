231df7af0fd8dbf115262bac1e895c3055962309
Ozan Eğitmen <ozanegitmen@gmail.com>
2018-09-10 10:04:01 +0300

    Do more refactoring

--- fc2.ino
+++ fc2.ino
@@ -1,11 +1,27 @@
+/*
+ * Copyright (C) 2018 Ozan Egitmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_BMP085.h>
 #include <MPU6050_tockn.h>
 
-#define SEPERATION_VELOCITY 0.3
+#define SEPERATION_ACCELERATION 0.2
 #define PARACHUTE_ALTITUDE 25
 
+#define DEBUG 1
 #define BAUD_RATE 9600
 #define SD_PORT 10
 
@@ -29,77 +45,75 @@ void setup()
         return;
     }
     mpu.begin();
+#ifdef DEBUG
     mpu.calcGyroOffsets(true);
+#else
+    mpu.calcGyroOffsets(false);
+#endif
     mpu.update();
 
     basePressure = bmp.readPressure();
     baseAcceleration = pow(pow(mpu.getAccX(), 2) + pow(mpu.getAccY(), 2) + pow(mpu.getAccZ(), 2), 0.5);
 
+    float gyroXOffset = mpu.getGyroXoffset();
+    float gyroYOffset = mpu.getGyroYoffset();
+    float gyroZOffset = mpu.getGyroZoffset();
+
+#ifdef DEBUG
+    Serial.print("basePressure = ");
+    Serial.println(basePressure);
+    Serial.print("baseAcceleration = ");
+    Serial.println(baseAcceleration);
+    Serial.print("gyroOffset = [");
+    Serial.print(gyroXOffset);
+    Serial.print(", ");
+    Serial.print(gyroYOffset);
+    Serial.print(", ");
+    Serial.print(gyroZOffset);
+    Serial.println("]");
+#endif
+
     logFile = SD.open("FLT_LOG.BIN", FILE_WRITE);
-    logFile.print("\n--------------------\nFlight data:\nbasePressure = ");
-    logFile.print(basePressure);
-    logFile.print("\nbaseAcceleration = ");
-    logFile.print(baseAcceleration);
-    logFile.print("\ngyroOffset = [");
-    logFile.print(mpu.getGyroXoffset());
-    logFile.print(", ");
-    logFile.print(mpu.getGyroYoffset());
-    logFile.print(", ");
-    logFile.print(mpu.getGyroZoffset());
-    logFile.print("]\n");
-    logFile.print("Everything after this is binary data.\n--------------------\n");
+    uint8_t buf[28] = {0, 0, 0, 0, 0, 0, 0, 0};
+    memcpy(&buf[8], &basePressure, 4);
+    memcpy(&buf[12], &baseAcceleration, 4);
+    memcpy(&buf[16], &gyroXOffset, 4);
+    memcpy(&buf[20], &gyroYOffset, 4);
+    memcpy(&buf[24], &gyroZOffset, 4);
+    logFile.write(buf, 28);
     logFile.flush();
 }
 
 bool stageSeperated = false;
 bool parachuteDeployed = false;
-bool inFlight = false;
-float lastAltitudes[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float lastAltitudes[] = {0, 0, 0, 0, 0};
 
 void loop()
 {
+    unsigned long curTime = millis();
     float altitude = bmp.readAltitude(basePressure);
-    if (altitude < 15 && !inFlight)
-    {
-        return;
-    }
-    inFlight = true;
 
     mpu.update();
     float accX = mpu.getAccX();
     float accY = mpu.getAccY();
     float accZ = mpu.getAccZ();
     float acceleration = pow(square(accX) + square(accY) + square(accZ), 0.5) - baseAcceleration;
+
+#ifdef DEBUG
     Serial.print("altitude=");
     Serial.print(altitude);
     Serial.print("\t\tacceleration=");
-    Serial.print(acceleration);
-    Serial.print("\n");
+    Serial.println(acceleration);
+#endif
 
-    // TODO: Get lastAltitudes avg and compare it to lastAltitude
-    if (!stageSeperated && acceleration < SEPERATION_VELOCITY && lastAltitude > altitude)
-    {
-        Serial.println("Seperating stage 2");
-        // TODO: Seperate stage 2
-        stageSeperated = true;
-    }
-    // TODO: Push & shift to lastAltitudes
-    lastAltitude = altitude;
-
-    if (!parachuteDeployed && stageSeperated && altitude < PARACHUTE_ALTITUDE)
-    {
-        Serial.println("Deploying parachute");
-        // TODO: Deploy parachute
-        parachuteDeployed = true;
-    }
-
-    unsigned long currentTime = millis();
     float angleX = mpu.getAngleX();
     float angleY = mpu.getAngleY();
     float angleZ = mpu.getAngleZ();
+    float bmpTemperature = bmp.readTemperature();
+    float mpuTemperature = mpu.getTemp();
 
-    uint8_t buf[34];
-    memcpy(&buf[0], &currentTime, 4);
+    uint8_t buf[42];
+    memcpy(&buf[0], &curTime, 4);
     memcpy(&buf[4], &altitude, 4);
     memcpy(&buf[8], &accX, 4);
     memcpy(&buf[12], &accY, 4);
@@ -107,8 +121,52 @@ void loop()
     memcpy(&buf[20], &angleX, 4);
     memcpy(&buf[24], &angleY, 4);
     memcpy(&buf[28], &angleZ, 4);
-    buf[32] = stageSeperated;
-    buf[33] = parachuteDeployed;
-    logFile.write(buf, 34);
+    memcpy(&buf[32], &bmpTemperature, 4);
+    memcpy(&buf[36], &mpuTemperature, 4);
+    buf[40] = stageSeperated;
+    buf[41] = parachuteDeployed;
+    logFile.write(buf, 42);
     logFile.flush();
+
+    if (altitude < 10)
+    {
+        return;
+    }
+
+    float avgOfLastAltitudes = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        avgOfLastAltitudes += lastAltitudes[i];
+    }
+    avgOfLastAltitudes /= 5;
+
+    if (!stageSeperated && acceleration < SEPERATION_ACCELERATION && altitude < avgOfLastAltitudes)
+    {
+#ifdef DEBUG
+        Serial.println("Seperating stage 2");
+#endif
+        // TODO: Seperate stage 2
+        stageSeperated = true;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        lastAltitudes[i] = lastAltitudes[i + 1];
+    }
+    lastAltitudes[4] = altitude;
+
+    if (!parachuteDeployed && stageSeperated && altitude < PARACHUTE_ALTITUDE)
+    {
+#ifdef DEBUG
+        Serial.println("Deploying parachute");
+#endif
+        // TODO: Deploy parachute
+        parachuteDeployed = true;
+    }
+
+#ifdef DEBUG
+    unsigned long lastTime = millis();
+    Serial.print("loop time: ");
+    Serial.println(lastTime - curTime);
+#endif
 }
--- read_flt_log.py
+++ read_flt_log.py
@@ -1,26 +1,42 @@
+# Copyright (C) 2018 Ozan Egitmen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import struct
 
 flightConfig = "E:/FLT_LOG.BIN"
 
 
-def getf(data):
-    return struct.unpack("f", data)[0]
+def getf(handle):
+    return struct.unpack("f", handle.read(4))[0]
 
 
 with open(flightConfig, mode='rb') as f:
-    while f.readable:
-        print("currentTime =", int.from_bytes(f.read(4), byteorder='little'))
-        print("altitude =", getf(f.read(4)))
-        accX = getf(f.read(4))
-        print("accX =", accX)
-        accY = getf(f.read(4))
-        print("accY =", accX)
-        accZ = getf(f.read(4))
-        print("accZ =", accX)
-        print("calculated acceleration =", pow(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2), 0.5))
-        print("angleX =", getf(f.read(4)))
-        print("angleY =", getf(f.read(4)))
-        print("angleZ =", getf(f.read(4)))
-        print("stageSeperated =", f.read(1) == 1)
-        print("parachuteDeployed =", f.read(1) == 1)
-        print("\n")
+
+    # TODO: Read the 28 initial bytes, then read below untill we get 8 null, then repeat
+    print("currentTime =", int.from_bytes(f.read(4), byteorder='little', signed=False))
+    print("altitude =", getf(f))
+    accX = getf(f)
+    print("accX =", accX)
+    accY = getf(f)
+    print("accY =", accX)
+    accZ = getf(f)
+    print("accZ =", accX)
+    print("acceleration =", pow(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2), 0.5))
+    print("angleX =", getf(f))
+    print("angleY =", getf(f))
+    print("angleZ =", getf(f))
+    print("bmpTemperature =", getf(f))
+    print("mpuTemperature =", getf(f))
+    print("stageSeperated =", f.read(1) == 1)
+    print("parachuteDeployed =", f.read(1) == 1)
+    print("\n")
